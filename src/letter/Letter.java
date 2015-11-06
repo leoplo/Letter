14945602bb4c3fafdecad2a3b0f6a9d4c5b2e6e5
Geneau Leo-paul <leo-paul.geneau@univ-lille1.fr>
2015-11-06 11:39:55 +0100

    Letter beginned

--- src/letter/Letter.java
+++ src/letter/Letter.java
@@ -1,5 +1,17 @@
 package letter;
 
-public abstract class Letter <T extends Content> {
+import letter.content.Content;
 
+public abstract class Letter<T extends Content> {
+	
+	protected T content;
+	protected int cost;
+	protected Inhabitant sender;
+	protected Inhabitant receiver;
+	
+	public Letter (T content){
+		
+	}
+	
+	public abstract action();
 }
